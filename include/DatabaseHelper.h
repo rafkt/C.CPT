43ac6ae191fcb62d54c896e15625b8b15ed71640
Rafael Ktistakis <rafaelktistakis@gmail.com>
2016-11-23 15:37:20 +0000

    Finished databaseHelper

--- include/DatabaseHelper.h
+++ include/DatabaseHelper.h
@@ -7,13 +7,12 @@
 
 class DatabaseHelper{
 	public:
-		enum Format{SPMF, QUEST};
+		enum Format{SPMF};
 		DatabaseHelper(std::string, Format); //use as  DatabaseHelper db(fileName, DatabaseHelper::SPMF);
 		~DatabaseHelper();
 		std::vector<Sequence*> getDatabase();
 	private:
-		std::vector<Sequence*> loadSPMFFormat(std::string);
-		std::vector<Sequence*> loadQuestFormat(std::string);
+		void loadSPMFFormat(std::string);
 		std::string getFullPath(std::string);
 		std::vector<Sequence*> _database;
 };
--- src/SequenceDatabase/DatabaseHelper.cpp
+++ src/SequenceDatabase/DatabaseHelper.cpp
@@ -1,32 +1,38 @@
 #include "../../include/DatabaseHelper.h"
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 
 using namespace std;
 
 
-DatabaseHelper::DatabaseHelper(string, Format){
+DatabaseHelper::DatabaseHelper(string filename, Format fm){
 
 }
 DatabaseHelper::~DatabaseHelper(){
 
 }
 vector<Sequence*> DatabaseHelper::getDatabase(){
-	vector<Sequence*> v;
-	v.push_back(nullptr);
-	return v;
+	return _database;
 }
-vector<Sequence*> DatabaseHelper::loadSPMFFormat(string){
-	vector<Sequence*> v;
-	v.push_back(nullptr);
-	return v;
+void DatabaseHelper::loadSPMFFormat(string filename){
+	ifstream file(getFullPath(filename));
+	string line;
+	while(getline(file, line)){
+		stringstream linestream(line);
+		vector<uint64_t> tmp_v;
+		for(string word; linestream >> word;){
+			uint64_t tmp = atoi(word.c_str());
+			//reading from quest datasets
+			if (tmp == -1 || tmp == -2) continue;
+			tmp_v.push_back(tmp);
+		}
+		Sequence* s = new Sequence(tmp_v);
+		_database.push_back(s);
+	}
 }
-vector<Sequence*> DatabaseHelper::loadQuestFormat(string){
-	vector<Sequence*> v;
-	v.push_back(nullptr);
-	return v;
-}
-string DatabaseHelper::getFullPath(string){
-	return "";
+string DatabaseHelper::getFullPath(string filename){
+	return "./Datasets/" + filename;
 }
