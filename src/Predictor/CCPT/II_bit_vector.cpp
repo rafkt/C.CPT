c9619b4c4cf40dac2a22921ce7cf10be2104c2be
Rafael Ktistakis <rafaelktistakis@gmail.com>
2016-11-24 19:08:48 +0000

    Debugging II_bit_vector - for the Bible dataset, quering the whole dataset should give back 39268 answers or 5801 for quering the first 5000 sequences

--- src/Predictor/CCPT/II_bit_vector.cpp
+++ src/Predictor/CCPT/II_bit_vector.cpp
@@ -1,5 +1,6 @@
 #include "../../../include/II_bit_vector.h"
 #include "../../../include/Sequence.h"
+#include "../../../include/DatabaseHelper.h"
 #include <iostream>
 
 using namespace std;
@@ -23,17 +24,17 @@ II_bit_vector::~II_bit_vector(){
 }
 
 uint64_t* II_bit_vector::query(uint64_t* items, uint64_t size){
-	unsigned int bitvectorRawSize = sequenceNumber / 64;
+	uint64_t bitvectorRawSize = sequenceNumber / 64;
 	bitvectorRawSize = sequenceNumber%64 != 0 ? bitvectorRawSize + 1 : bitvectorRawSize;
 
 	uint64_t* results = new uint64_t[bitvectorRawSize];
-	for (int j = 0; j < bitvectorRawSize; j++) results[j] = (uint64_t)-1;
+	for (uint64_t j = 0; j < bitvectorRawSize; j++) results[j] = (uint64_t)-1;
 
-	for (int i = 0; i < size; i++) {
+	for (uint64_t i = 0; i < size; i++) {
 
 		uint64_t* tmp_bv = bit_vectors_table[items[i]];
 		//uint64_t* tmp_bvRawData = tmp_bv.data();
-		for (int j = 0; j < bitvectorRawSize; j++) { // for every uint64_t of the bit_vector make a bitwise-and with the result bit_vector
+		for (uint64_t j = 0; j < bitvectorRawSize; j++) { // for every uint64_t of the bit_vector make a bitwise-and with the result bit_vector
 			results[j] = results[j] & tmp_bv[j];
 		}
 	}
@@ -48,26 +49,43 @@ uint64_t II_bit_vector::getCardinality(uint64_t item){
 	return cardinalityCounter;
 }
 
-// int main(){
+int main(){
 
-// 	vector<uint64_t> seq1 = {1, 2, 3, 4, 5};
-// 	Sequence* s1 = new Sequence(seq1);
+	// vector<uint64_t> seq1 = {1, 2, 3, 4, 5};
+	// Sequence* s1 = new Sequence(seq1);
 
-// 	vector<uint64_t> seq2 = {6, 7, 8, 1};
-// 	Sequence* s2 = new Sequence(seq2);
+	// vector<uint64_t> seq2 = {6, 7, 8, 1};
+	// Sequence* s2 = new Sequence(seq2);
 
 
-// 	vector<uint64_t> seq3 = {10, 1, 12};
-// 	Sequence* s3 = new Sequence(seq3);
+	// vector<uint64_t> seq3 = {10, 1, 12};
+	// Sequence* s3 = new Sequence(seq3);
 
-// 	std::vector<Sequence*> v = {s1, s2, s3};
+	// std::vector<Sequence*> v = {s1, s2, s3};
 
-// 	InvertedIndex* II = new II_bit_vector(v);
+	DatabaseHelper db("BIBLE.txt", DatabaseHelper::TXT);
+	// for(uint64_t i = 0; i < db.getDatabase().size(); i++){
+	// 	db.getDatabase()[i]->print();
+	// }
 
-// 	delete II;
-// 	delete s1;
-// 	delete s2;
-// 	delete s3;
+	InvertedIndex* II = new II_bit_vector(db.getDatabase());
+	uint64_t counter = 0;
+	for(uint64_t i = 0; i < db.getDatabase().size(); i++){
+		if (i == 1) break;
+	 	uint64_t* answer = II->query(db.getDatabase()[i]->getItems(), db.getDatabase()[i]->size());
+	 	for (uint64_t  j = 0; j < II->getSequenceNumber(); j++)
+			if ((answer[(j) / 64] >> ((j) % 64)) & 1) counter++;
+		delete[] answer;
+	 }
 
-// 	return 0;
-// }
+	 cout << db.getDatabase().size() << endl;
+
+	 cout << counter << endl;
+
+	delete II;
+	// delete s1;
+	// delete s2;
+	// delete s3;
+
+	return 0;
+}
