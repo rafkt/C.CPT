dc95bf641da5f2c106dad1a8404f446282787e09
Rafael Ktistakis <rafaelktistakis@gmail.com>
2016-12-05 21:53:52 +0000

    Continue by implementing CPT_SD_Tree

--- include/CPT_SD_Tree.h
+++ include/CPT_SD_Tree.h
@@ -5,13 +5,13 @@
 #define CPT_SD_TREE_H
 class  CPT_SD_Tree : public PredictionTree{
 	public:
-		CPT_SD_Tree();
-		CPT_SD_Tree(uint64_t);
+		CPT_SD_Tree(PredictionTree*, PredictionTree**, uint64_t);
 		~CPT_SD_Tree();
 		void addChild(uint64_t);
 		bool hasChild(uint64_t);
 		PredictionTree* getChild(uint64_t);
 		PredictionTree* getParent();
+		std::vector<uint64_t> getNodesToRoot();
 		uint64_t getItem();
 };
 #endif
--- src/Predictor/CCPT/CPT_SD_Tree.cpp
+++ src/Predictor/CCPT/CPT_SD_Tree.cpp
@@ -1,29 +1,38 @@
 #include "../../../include/CPT_SD_Tree.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-CPT_SD_Tree::CPT_SD_Tree() : PredictionTree(){
-
-}
-CPT_SD_Tree::CPT_SD_Tree(uint64_t item) : PredictionTree(item){
-
+CPT_SD_Tree::CPT_SD_Tree(PredictionTree* trie, PredictionTree** LT, uint64_t nodeNumber){
+//good idea to have a node number attribute in every node of trie;
+//no need to set up this in advance since the tree will be parced here in Level order - so we can assign the attribute here. Alternatively along with the k-ary bitstring create a vector of PredictionTree*. This will not affect the current memory of CPT_Trie.
+//Do next - print a tries nodes in Level order - create main
 }
 CPT_SD_Tree::~CPT_SD_Tree(){
 
 }
 PredictionTree* CPT_SD_Tree::getParent(){
+	cerr << "CPT_SD_TREE (getParent): Operation Not supported" << endl;
 	return nullptr;
 }
-
-void CPT_SD_Tree::addChild(uint64_t item){
+vector<uint64_t> CPT_SD_Tree::getNodesToRoot(){
 	
+	vector<uint64_t> v;
+	return v;
+}
+void CPT_SD_Tree::addChild(uint64_t item){
+	cerr << "CPT_SD_TREE (addChild): Operation Not supported" << endl;	
 }
 bool CPT_SD_Tree::hasChild(uint64_t item){
+	cerr << "CPT_SD_TREE (hasChild): Operation Not supported" << endl;
 	return false;
 }
 PredictionTree* CPT_SD_Tree::getChild(uint64_t item){
+	cerr << "CPT_SD_TREE (getChild): Operation Not supported" << endl;
 	return nullptr;
 }
 uint64_t CPT_SD_Tree::getItem(){
+	cerr << "CPT_SD_TREE (getItem): Operation Not supported" << endl;
 	return 0;
 }
